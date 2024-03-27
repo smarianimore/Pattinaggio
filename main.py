0972a0428fa86c563ce54497e89321ea465bcecc
Stefano Mariani <stefano.mariani@unimore.it>
2024-03-27 23:46:25 +0100

    fixed up to 4 excluded

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # This is a sample Python script.
+import json
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -8,13 +9,19 @@ import math
 
 pd.set_option('display.max_columns', None)
 
-excel_file_path = "/mnt/c/Users/stefa/OneDrive/Documents/2023_05_06-Trofeo_Primavera_2.xlsx"
-sheet_name = "test"
-n_skaters = 5
+###############################
+# FOGLIO DATI E NUMERO ATLETE #
+###############################
 
-header_row_idx = 1
-columns = "A:J"
-decimal = ','
+excel_file_path = "/mnt/c/Users/stefa/OneDrive/Documents/Pattinaggio-Lugo-20240303.xlsx"
+sheet_name = "promo B femminile"
+header_row_idx = 0  # which row contains the header
+data_columns = "A:N"
+header_score_1 = "DIFFICOLTA"
+header_score_2 = "STILE"
+entrance_order = "INGRESSO"
+n_skaters = 11
+decimal_separator = ','
 
 n_judges = 3
 if n_judges == 3:
@@ -27,60 +34,49 @@ else:
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     df = pd.read_excel(excel_file_path,
-                             sheet_name,
-                             header=header_row_idx,
-                             usecols=columns,
-                             nrows=n_skaters,
-                             decimal=decimal)
+                       sheet_name,
+                       header=header_row_idx,
+                       usecols=data_columns,
+                       nrows=n_skaters,
+                       decimal=decimal_separator)
 
-    ################################
-    # AGGIUNTA PUNTEGGIO A DATASET #
-    ################################
-    #print(df.head(n_skaters))
-    for idx in range(1, n_judges+1):
-        df[f'Totale {idx}'] = df[f'CONTENUTO TECNICO {idx}'] + df[f'CONTENUTO ARTISTICO {idx}']
-    df['Totale'] = sum([df[f'Totale {idx}'] for idx in range(1, n_judges+1)])
-    #print(df.head(n_skaters))
+    print(df.head(n_skaters))
 
     #################################
     # 1-2) CALCOLO MATRICE VITTORIE #
     #################################
-    victories = []
+
+    victories_matrix = {}  # dict with {skater: {competitor: victories}}
     for reference in df.itertuples():
-        victories.append([])
+        victories_matrix[int(df.loc[reference.Index, entrance_order])] = {}
+        print(f"{df.loc[reference.Index, entrance_order]} vs:")
         for competitor in df.itertuples():
-            victory = 0
-            if df.loc[reference.Index, 'ORDINE INGRESSO IN PISTA'] == df.loc[competitor.Index, 'ORDINE INGRESSO IN PISTA']:  # comparing skater with herself
-                victory = -1
-            else:
-                # print(f"reference={df.loc[reference.Index, 'COGNOME']} {df.loc[reference.Index, 'NOME']}, "
-                #       f"competitor={df.loc[competitor.Index, 'COGNOME']} {df.loc[competitor.Index, 'NOME']}")
+            victories = 0
+            if df.loc[reference.Index, entrance_order] != df.loc[competitor.Index, entrance_order]:  # NOT comparing skater with herself
                 for idx in range(1, n_judges + 1):
-                    if df.loc[reference.Index, f'Totale {idx}'] > df.loc[competitor.Index, f'Totale {idx}']:
-                        victory += 1
-                    elif math.isclose(df.loc[reference.Index, f'Totale {idx}'], df.loc[competitor.Index, f'Totale {idx}']):
-                        victory += 0.5
-            victories[-1].append({
-                'ref': f"{df.loc[reference.Index, 'COGNOME']} {df.loc[reference.Index, 'NOME']}",
-                'vs': f"{df.loc[competitor.Index, 'COGNOME']} {df.loc[competitor.Index, 'NOME']}",
-                'victory': victory
-            })
+                    if math.isclose(df.loc[reference.Index, f'TOTALE {idx}'], df.loc[competitor.Index, f'TOTALE {idx}']):
+                        victories += 0.5
+                    elif df.loc[reference.Index, f'TOTALE {idx}'] > df.loc[competitor.Index, f'TOTALE {idx}']:
+                        victories += 1
+                victories_matrix[int(df.loc[reference.Index, entrance_order])][int(df.loc[competitor.Index, entrance_order])] = victories
+                print(f"\t{df.loc[competitor.Index, entrance_order]}: {victories}", end="")
+        print()
+    #print(json.dumps(victories_matrix, indent=4))
 
     ######################################
     # 3) CALCOLO VITTORIE DI MAGGIORANZA #
     ######################################
-    majorities = []
-    for ref in range(len(victories)):
+
+    majorities = {}  # dict with {skater: majority score}
+    for ref in victories_matrix:
         maj = 0
-        for vs in range(len(victories[ref])):
-            if victories[ref][vs]['victory'] > majority:
-                maj += 1
-            elif math.isclose(victories[ref][vs]['victory'], majority):
+        for vs in victories_matrix[ref]:
+            if math.isclose(victories_matrix[ref][vs], majority):
                 maj += 0.5
-        majorities.append({
-            'id': ref+1,
-            'majority': maj
-        })
+            elif victories_matrix[ref][vs] > majority:
+                maj += 1
+        majorities[ref] = maj
+    print(f"{majorities=}")
 
     ################################################
     # 4) CALCOLO PIAZZAMENTO (parimeriti possibili #
@@ -132,6 +128,7 @@ if __name__ == '__main__':
                     }
                 )
     print(f"{parities=}")
+    print(f"{separate_victories}")
     for maj in separate_victories:  # for each parity (same majority), order those peers according to vs. wins
         print(f"{maj=}", end=") ")
         sub_sep_vic = sorted(separate_victories[maj], key=lambda d: d['wins'], reverse=True)
@@ -144,7 +141,7 @@ if __name__ == '__main__':
     for p in placement:
         if p['majority'] not in separate_victories:
             print(f"{standing}°) {df.loc[p['id']-1, 'NOME']} {df.loc[p['id']-1, 'COGNOME']} "
-                  f"(#pista: {df.loc[p['id']-1, 'ORDINE INGRESSO IN PISTA']}, punteggio: {df.loc[p['id']-1, 'Totale']})")
+                  f"(#pista: {df.loc[p['id']-1, entrance_order]}, punteggio: {df.loc[p['id'] - 1, 'Totale']})")
             standing += 1
         else:
             if p['majority'] not in skip:
@@ -152,7 +149,7 @@ if __name__ == '__main__':
                 for e in separate_victories[p['majority']]:
                     print(
                         f"{standing}°) {df.loc[placement[e['peer']]['id']-1, 'NOME']} {df.loc[placement[e['peer']]['id']-1, 'COGNOME']} "
-                        f"(#pista: {df.loc[placement[e['peer']]['id']-1, 'ORDINE INGRESSO IN PISTA']}, punteggio: {df.loc[placement[e['peer']]['id']-1, 'Totale']})")
+                        f"(#pista: {df.loc[placement[e['peer']]['id']-1, entrance_order]}, punteggio: {df.loc[placement[e['peer']]['id'] - 1, 'Totale']})")
                     standing += 1
     print("<========== STANDINGS <==========")
 
@@ -160,7 +157,17 @@ if __name__ == '__main__':
     # 6) CALCOLO PIAZZAMENTO (parimeriti impossibili) #
     ###################################################
 
-
+    more_parities = {}  # dict of {majorities: {wins: [peers]}} --- peer is idx of placement (sorted) list!
+    for m in separate_victories:
+        more_parities[m] = {}
+        for idx_ref in range(len(separate_victories[m])):
+            for idx_other in range(len(separate_victories[m])):
+                if idx_ref != idx_other and math.isclose(separate_victories[m][idx_ref]['wins'], separate_victories[m][idx_other]['wins']):
+                    if separate_victories[m][idx_ref]['wins'] not in more_parities[m]:
+                        more_parities[m][separate_victories[m][idx_ref]['wins']] = [separate_victories[m][idx_ref]['peer']]
+                    if separate_victories[m][idx_other]['peer'] not in more_parities[m][separate_victories[m][idx_ref]['wins']]:
+                        more_parities[m][separate_victories[m][idx_ref]['wins']].append(separate_victories[m][idx_other]['peer'])
+    print(f"{more_parities}")
 
     #########################################
     # VISUALIZZAZIONE CLASSIFICA DEFINITIVA #
