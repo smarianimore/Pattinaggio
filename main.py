e7abe54f86ed2630a08a7b42ed16e467d07d8210
Stefano Mariani <stefano.mariani@unimore.it>
2024-04-07 00:20:47 +0200

    fixed headers

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ pd.set_option('display.max_columns', None)
 # CONFIGURAZIONE FOGLIO DATI E NUMERO ATLETE #
 ##############################################
 
-excel_file_path = "/mnt/c/Users/stefa/OneDrive/Documents/Pattinaggio-Lugo-20240303.xlsx"
-sheet_name = "test parimeriti"  # "promo B femminile"
-header_row_idx = 0  # which row contains the header
+excel_file_path = "/mnt/c/Users/stefa/OneDrive/Documents/2024-trofeo-primavera.xlsx"
+sheet_name = "AZZURRI START (M)"  # "promo B femminile"
+header_row_idx = 1  # which row contains the header
 data_columns = "A:N"
 header_score_tech = "DIFFICOLTA"
 header_score_art = "STILE"
@@ -53,16 +53,16 @@ if __name__ == '__main__':
         victories_matrix[int(df.loc[skater_ref.Index, entrance_order])] = {}
         print(f"{df.loc[skater_ref.Index, entrance_order]} vs:")
         for skater_vs in df.itertuples():
-            victories = 0
+            victories = 0.0
             if df.loc[skater_ref.Index, entrance_order] != df.loc[skater_vs.Index, entrance_order]:  # NOT comparing skater with herself
                 for idx in range(1, n_judges + 1):
                     if math.isclose(df.loc[skater_ref.Index, f'TOTALE {idx}'], df.loc[skater_vs.Index, f'TOTALE {idx}']):
                         if math.isclose(df.loc[skater_ref.Index, f'{header_score_art} {idx}'], df.loc[skater_vs.Index, f'{header_score_art} {idx}']):
                             victories += 0.5
                         elif df.loc[skater_ref.Index, f'{header_score_art} {idx}'] > df.loc[skater_vs.Index, f'{header_score_art} {idx}']:
-                            victories += 1
+                            victories += 1.0
                     elif df.loc[skater_ref.Index, f'TOTALE {idx}'] > df.loc[skater_vs.Index, f'TOTALE {idx}']:
-                        victories += 1
+                        victories += 1.0
                 victories_matrix[int(df.loc[skater_ref.Index, entrance_order])][int(df.loc[skater_vs.Index, entrance_order])] = victories
                 print(f"\t{df.loc[skater_vs.Index, entrance_order]}: {victories}", end="")
         print()
@@ -74,12 +74,12 @@ if __name__ == '__main__':
 
     majorities_per_skater = {}  # dict with {skater: majority score}
     for skater_ref in victories_matrix:
-        majorities = 0
+        majorities = 0.0
         for skater_vs in victories_matrix[skater_ref]:
             if math.isclose(victories_matrix[skater_ref][skater_vs], majority_threshold):
                 majorities += 0.5
             elif victories_matrix[skater_ref][skater_vs] > majority_threshold:
-                majorities += 1
+                majorities += 1.0
         majorities_per_skater[skater_ref] = majorities
     print(f"{majorities_per_skater=}")
 
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     print(f"{standings_majorities=}")
     print("==========> Classifica per voti di maggioranza (step 4):")
     for pos, skater in zip(range(1, len(standings_majorities) + 1), standings_majorities):
-        tot_rounded = Decimal(df.loc[skater[0]-1, 'TOTALE'])
+        tot_rounded = Decimal(float(df.loc[skater[0]-1, 'TOTALE']))
         tot_rounded = tot_rounded.quantize(Decimal('1.00'), rounding=ROUND_HALF_UP)
         print(f"{pos}°) {df.loc[skater[0]-1, 'NOME']} {df.loc[skater[0]-1, 'COGNOME']} "
               f"(#ingresso: {df.loc[skater[0]-1, entrance_order]}, majorities: {skater[1]}, punteggio: {tot_rounded})")
@@ -105,7 +105,7 @@ if __name__ == '__main__':
     separate_victories_per_skater = {}  # dict with {skater: (victories, [competitors])}
     first = -1
     for skater_ref in majorities_per_skater:
-        victories = 0
+        victories = 0.0
         competitors = []
         for skater_vs in majorities_per_skater:
             if skater_ref != skater_vs and math.isclose(majorities_per_skater[skater_ref], majorities_per_skater[skater_vs]):
@@ -113,15 +113,15 @@ if __name__ == '__main__':
                     first = skater_ref
                 victories += victories_matrix[skater_ref][skater_vs]
                 competitors.append(skater_vs)
-        if victories > 0:
+        if victories > 0.0:
             separate_victories_per_skater[skater_ref] = (victories, competitors)
 
     if first != -1:
         print(f"{separate_victories_per_skater=}")
         standings_peer_majorities = sorted(separate_victories_per_skater.items(), key=lambda x: x[1][0], reverse=True)
         print(f"{standings_peer_majorities=}")
-        #standings_peer_majorities = [skater[0] for skater in standings_peer_majorities]
-        standings_peer_majorities = [10, 11, 9]  # force test
+        standings_peer_majorities = [skater[0] for skater in standings_peer_majorities]
+        #standings_peer_majorities = [10, 11, 9]  # force test
         print(f"{standings_peer_majorities=}")
 
         fix_point = standings_majorities.index(first)
@@ -130,7 +130,7 @@ if __name__ == '__main__':
         print(f"{standings_majorities=}")
         print("==========> Classifica con parimeriti per voti di maggioranza risolti (step 5):")
         for pos, skater in zip(range(1, len(standings_majorities) + 1), standings_majorities):
-            tot_rounded = Decimal(df.loc[skater-1, 'TOTALE'])
+            tot_rounded = Decimal(float(df.loc[skater-1, 'TOTALE']))
             tot_rounded = tot_rounded.quantize(Decimal('1.00'), rounding=ROUND_HALF_UP)
             print(f"{pos}°) {df.loc[skater-1, 'NOME']} {df.loc[skater-1, 'COGNOME']} "
                   f"(#ingresso: {df.loc[skater-1, entrance_order]}, majorities: {majorities_per_skater[skater]}, punteggio: {tot_rounded})")
